7f6e782b044ae0a7f7d5148402f47b7e1a2d4432
Brumazzi <brumazzi_daniel@yahoo.com.br>
2016-02-13 04:01:20 -0300

    reduce functions

--- share/wifi-lan/functions.sh
+++ share/wifi-lan/functions.sh
@@ -14,39 +14,36 @@ range(){
 		export parm="$x"
 	done
 
-	while [ $s -le $e ]; do
+	while [ $s -lt $e ]; do
 		echo -n "$s "
 		let s=$s+1
 	done
 }
 
 reconnect(){
-	loop=1
-	while [ "$loop" == 1 ]; do
-		loop=0
+	while [ "loop" ]; do
+		on=$(ps aux | grep wpa_su | wc -l)
+		[[ "$on" -gt "1" ]] &&
+			killall -s TERM wpa_supplicant -w &&
+			[[ "$(ifconfig | grep $dev)" ]] &&
+				ifconfig $dev up
 		pos=0
-		essids=$(iwlist scanning | grep ESSID)
-		for x in $essids; do
-			let len="$(python /usr/share/wifi-lan/strlen $x)-1"
-			ESSID[$pos]=$(echo $x | cut -c8-$len)
+		essids=$(iwlist $1 scanning | grep ESSID)
+		for x in $(echo $essids | tr '"' '\n' | grep -v ':'); do
+			ESSID[$pos]=$x
 			let pos=$pos+1
 		done
 
 		let pos=$pos-1
-		LINKS=$(ls etc/wifi_lan/)
+		LINKS=$(ls /etc/wifi_lan/)
 		for x in $LINKS; do
 			for y in $(range -s 0 -e $pos); do
-				if [ "$x" == "${ESSID[$pos]}" ]; then
-					ifconfig wlan0 up &&
-						wpa_supplicant -i wlan0 -c ~/.wifi_lan/$x -BD wext &&
-						return 0
+				if [ "$x" == "${ESSID[$y]}" ]; then
 					ifconfig wlp3s0 up &&
-						wpa_supplicant -i wlp3s0 -c ~/.wifi_lan/$x -BD wext &&
+						wpa_supplicant -i $1 -c /etc/wifi_lan/$x -BD wext &&
 						return 0
 					echo "Wifi-Device not found." &&
 					return 1
-
-					loop=1
 				fi
 			done
 		done
